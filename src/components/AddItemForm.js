06fc7d96e9615364495e1e4a21c628135c97d260
Damien <mail@damientimewell.com>
2020-01-15 17:39:32 +0100

    Wednesday codealong

---
+++ src/components/AddItemForm.js
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { fridge } from 'reducers/fridge'
+
+export const AddItemForm = () => {
+  const [name, setName] = useState('')
+  const dispatch = useDispatch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(fridge.actions.addItem(name))
+    setName('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+      </label>
+
+      <button type="submit">Add item</button>
+    </form>
+  )
+}
